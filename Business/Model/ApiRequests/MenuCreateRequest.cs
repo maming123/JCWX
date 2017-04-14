75624f0d25331dba9994a968014a6f0afea8cbc7
maming123 <hurricanefbi@163.com>
2017-04-14 22:10:19 +0800

    公众号SDK来自JCWX

---
+++ Business/Model/ApiRequests/MenuCreateRequest.cs
@@ -0,0 +1,52 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using WX.Model.ApiResponses;
+using WX.Model.Exceptions;
+
+namespace WX.Model.ApiRequests
+{
+    public class MenuCreateRequest : ApiRequest<MenuCreateResponse>
+    {
+        [JsonProperty("button")]
+        public IEnumerable<ClickButton> Buttons { get; set; }
+
+        internal override string Method
+        {
+            get { return "POST"; }
+        }
+
+        protected override string UrlFormat
+        {
+            get { return "https://api.weixin.qq.com/cgi-bin/menu/create?access_token={0}"; }
+        }
+
+        internal override string GetUrl()
+        {
+            return String.Format(UrlFormat, AccessToken);
+        }
+
+        protected override bool NeedToken
+        {
+            get { return true; }
+        }
+
+        public override string GetPostContent()
+        {
+            string str = JsonConvert.SerializeObject(this);
+            return str;
+        }
+
+        internal override void Validate()
+        {
+            base.Validate();
+
+            if (Buttons == null)
+            {
+                throw new ArgumentNullException("button is null");
+            }
+        }
+    }
+}
---
+++ CustomClickMenu/App_Code/DataGridRow.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CustomClickMenu.App_Code
+{
+    public class DataGridRow
+    {
+        public string Title { get; set; }
+
+        public string Key { get; set; }
+
+        public string Url { get; set; }
+
+        public string Id { get; set; }
+
+        public string RootId { get; set; }
+
+        public string MenuType { get; set; }
+        public string Media_ID { get; set; }
+    }
+}
